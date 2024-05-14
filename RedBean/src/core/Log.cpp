4b5b677443329a23bc6cc025d01516b9a186e086
Hongbin Miao <zslrmhb@gmail.com>
2024-05-14 01:36:21 -0700

    a

--- RedBean/src/core/Log.cpp
+++ RedBean/src/core/Log.cpp
@@ -2,11 +2,19 @@
 
 namespace RedBean
 {
-    Log::Log()
+    // Log::Log()
+    // {
+    // }
+
+    // Log::~Log()
+    // {
+    // }
+
+    std::shared_ptr<spdlog::logger> Log::s_CoreLogger;
+    std::shared_ptr<spdlog::logger> Log::s_ClientLogger;
+
+    void Log::init()
     {
     }
 
-    Log::~Log()
-    {
-    }
 }
--- RedBean/src/core/Log.h
+++ RedBean/src/core/Log.h
@@ -7,7 +7,22 @@ namespace RedBean
     class Log
     {
     public:
-        Log();
-        ~Log();
+        // Log();
+        // ~Log();
+
+        static void init();
+
+        inline static std::shared_ptr<spdlog::logger> &getCoreLogger()
+        {
+            return s_CoreLogger;
+        }
+        inline static std::shared_ptr<spdlog::logger> &getClientLogger()
+        {
+            return s_ClientLogger;
+        }
+
+    private:
+        static std::shared_ptr<spdlog::logger> s_CoreLogger;
+        static std::shared_ptr<spdlog::logger> s_ClientLogger;
     };
 }
